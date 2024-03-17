ba23968628fb4a37806261fc63affbdaf3ae099a
morgashen <mashudumorgan@gmail.com>
2024-03-17 03:13:39 -0700

    AirBnB_clone_v2

--- models/user.py
+++ models/user.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Defines the User class."""
-from models.base_model import BaseModel
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """Represent a User.
-
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-    """
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
+    email = Column('email', String(128), nullable=False)
+    password = Column('password', String(128), nullable=False)
+    first_name = Column('first_name', String(128), nullable=True, default="NULL")
+    last_name = Column('last_name', String(128), nullable=True, default="NULL")
+    # backref may need to be back_populates?Below line commented out bc console
+    # would not run with it in. This line was implemented in Task 8
+    places = relationship("Place", cascade="delete", backref="user")
+    # Below line is commented out for caution and was added in Task 9
+    reviews = relationship("Review", cascade="delete", backref="user")
